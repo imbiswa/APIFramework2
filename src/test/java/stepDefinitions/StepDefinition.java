b3f60c8fc60aae2f45442641fb50525393b3df23
R N Biswajit mallick <rnbiswajitmallick48@gmail.com>
2024-05-06 21:16:33 +0530

    lecture-94

--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -19,6 +19,7 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import pojo.AddPlace;
 import pojo.Location;
+import resources.APIResources;
 import resources.TestDataBuild;
 
 public class StepDefinition extends Utils{
@@ -26,6 +27,8 @@ public class StepDefinition extends Utils{
 	ResponseSpecification resspec;
 	Response response;
 	TestDataBuild data = new TestDataBuild();
+	//String place_id;
+	//JsonPath js;
 	
 	@Given(": Add place payload with {string} {string} {string}")
 	public void add_place_payload_with(String name, String language, String address) throws IOException{
@@ -34,19 +37,34 @@ public class StepDefinition extends Utils{
 		
 		 
 		 
-		resspec =new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+		//resspec =new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
 		 res=given().spec(requestSpecification())
 		.body(data.addPlacepayLoad(name, language, address));
 
 	    
 	}
 
-	@When(": user calls {string} with Post http request")
-	public void user_calls_with_post_http_request(String string) {
+	@When(": user calls {string} with {string} http request")
+	public void user_calls_with_http_request(String resource, String method) {
+	    // Write code here that turns the phrase above into concrete actions
 	    // Write code here that turns the phrase above into concrete actions
 		
-		response =res.when().post("/maps/api/place/add/json").
-				then().spec(resspec).extract().response();
+		APIResources resourceAPI=APIResources.valueOf(resource);//enum class constrcuter
+		System.out.println(resourceAPI.getResource());
+		
+		resspec =new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
+		
+		if(method.equalsIgnoreCase("POST"))
+		{
+		response =res.when().post(resourceAPI.getResource());
+				//then().spec(resspec).extract().response();
+		}
+		else if (method.equalsIgnoreCase("GET"))
+		{
+			response =res.when().post(resourceAPI.getResource());
+		}
+		
+
 
 	}
 
@@ -60,9 +78,20 @@ public class StepDefinition extends Utils{
 	@Then(": {string} in response body is {string}")
 	public void in_response_body_is(String keyValue, String Expectedvalue) {
 	    // Write code here that turns the phrase above into concrete actions
-	    String resp=response.asString();
-	    JsonPath js= new JsonPath(resp);
-	    assertEquals(js.get(keyValue).toString(),Expectedvalue);
+	    
+	    
+	    assertEquals(getJsonPath(response,keyValue),Expectedvalue);
+	}
+	
+	@Then(": Verify place_id created maps to {string} using {string}")
+	public void verify_place_id_created_maps_to_using(String expectedname, String resource) throws IOException {
+	    // Write code here that turns the phrase above into concrete actions
+		
+	   String place_id=getJsonPath(response,"place_id");
+	   res=given().spec(requestSpecification()).queryParam("place_id",place_id);
+	   user_calls_with_http_request(resource, "GET");
+	   String actualname =getJsonPath(response,"name");
+	   assertEquals(actualname,expectedname);
 	}
  
 
